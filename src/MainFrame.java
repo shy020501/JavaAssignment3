392cf6b4458c7171950fe0327a5a4f317db414d5
shy020501 <shy020501@naver.com>
2023-05-07 16:01:51 +0900

    Create Title Label

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,11 +3,11 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-    private int[] frameSize = {1000, 800};
+    private int[] frameSize = {1100, 800};
 
     public MainFrame(){
 
-        ImageIcon skkuWallpaper = new ImageIcon("src/images/skku_wallpaper.png"); // Wallpaper of the program
+        ImageIcon skkuWallpaper = new ImageIcon("src/images/skku_wallpaper.png"); // For storing wallpaper image
         Image skkuWallpaperImage = skkuWallpaper.getImage(); // Get image from image icon
 
         // Resize the image
@@ -26,6 +26,26 @@ public class MainFrame extends JFrame {
                 frameSize[1] / 1
         );
         add(wallPaperLabel);
+
+        ImageIcon title = new ImageIcon("src/images/title_label.png"); // For storing title image
+        Image titleImg = title.getImage(); // Get image from title icon
+
+        // Resize the image
+        titleImg = titleImg.getScaledInstance(
+                frameSize[0] * 31 / 60,
+                frameSize[1] / 10,
+                Image.SCALE_SMOOTH
+        );
+        title = new ImageIcon(titleImg); // Allocate resized image to title variable
+        JLabel titleLabel = new JLabel(title); // Create JLabel object to place the title image to screen
+        // Set bounds for wall paper label with respect to frame size
+        titleLabel.setBounds(
+                frameSize[0] / 40 + (frameSize[0] / 3) + frameSize[0] / 20,
+                frameSize[1] / 32,
+                frameSize[0] - ((frameSize[0] / 3) + frameSize[0] / 20) - 100,
+                frameSize[1] / 10
+        );
+        add(titleLabel);
 
 
         // Settings for frame
