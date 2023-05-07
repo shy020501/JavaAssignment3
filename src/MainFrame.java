26f5ee4ef09ad5c4211b6025efbeef8012903729
shy020501 <shy020501@naver.com>
2023-05-07 15:32:46 +0900

    Create Wallpaper for Main Frame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
     private int[] frameSize = {1000, 800};
 
     public MainFrame(){
+
+        ImageIcon skkuWallpaper = new ImageIcon("src/images/skku_wallpaper.png"); // Wallpaper of the program
+        Image skkuWallpaperImage = skkuWallpaper.getImage(); // Get image from image icon
+
+        // Resize the image
+        skkuWallpaperImage = skkuWallpaperImage.getScaledInstance(
+                (frameSize[0] / 3) + frameSize[0] / 20,
+                frameSize[1] - frameSize[1] / 8,
+                Image.SCALE_SMOOTH
+        );
+        skkuWallpaper = new ImageIcon(skkuWallpaperImage); // Allocate resized image to skkuWallPaper variable
+        JLabel wallPaperLabel = new JLabel(skkuWallpaper); // Create JLabel object to place the wallpaper to screen
+        // Set bounds for wall paper label with respect to frame size
+        wallPaperLabel.setBounds(
+                frameSize[0] / 40,
+                -frameSize[1] / 32,
+                frameSize[0] / 3,
+                frameSize[1] / 1
+        );
+        add(wallPaperLabel);
+
+
         // Settings for frame
         setTitle("SKKU New Student Enrollment");
         setSize(frameSize[0], frameSize[1]);
